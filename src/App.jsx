ff087de271fb9888df53b2eef5fdad71322d27db
sornmonika <monika.sorn@institute.pse.ngo>
2024-08-08 16:12:09 +0700

    createPage and InformationPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/core/DashboardPage";
-// import UserAccountPage from "./pages/userAccount/UserAccountPage";
 import BookCatalogPage from "./pages/bookCatalog/BookCatalogPage";
 import BookIssuePage from "./pages/bookIssue/BookIssuePage";
 import LoginPage from "./pages/auth/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
-// import UserAccountInfoPage from "./pages/userAccount/UserAccountInfoPage";
-// import CreateUserAccountPage from "./pages/userAccount/CreateUserAccountPage";
 import AppLayout from "./components/AppLayout";
 import MemberPage from "./pages/member/MemberPage";
 import UserAccountListPage from "./pages/userAcc/UserAcounts";
+import CreateMemberPage from "./pages/member/CreateMemberPage";
+import MemberInformationPage from "./pages/member/MemberInformationPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="*" element={<NotFoundPage />} />
+      
       <Route path="/login" element={<LoginPage />} />
       <Route element={<AppLayout />}>
         <Route path="/" element={<DashboardPage />} />
         <Route path="/user-account">
           <Route index element={<UserAccountListPage />} />
-          {/* <Route path="/book-catalog:id" element={<BookCatalogPage />} /> */}
         </Route>
         <Route path="/book-catalog" element={<BookCatalogPage />} />
         <Route path="/book-issue" element={<BookIssuePage />} />
-        <Route path="/member" element={<MemberPage />} />
+
+        <Route path="/member">
+          <Route index element={<MemberPage />} />
+          <Route path="create" element={ <CreateMemberPage /> }/>
+        </Route>
+        <Route path="/detail/:id" element={<MemberInformationPage />} />
       </Route>
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
--- src/pages/member/MemberPage.jsx
+++ src/pages/member/MemberPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { fetchMember } from "../../api/member";
+import { Link } from 'react-router-dom';
 
 const MemberPage = () => {
   const [members, setMember] = useState([]);
@@ -16,11 +17,16 @@ const MemberPage = () => {
   return (
     <>
       <h1 className="text-3xl font-bold mb-4">Member</h1>
-      <button class="text-xl py-2 px-7 bg-sky-500/100 text-white rounded-lg shadow-md hover:bg-sky-600 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75 mb-5">
-        Create
-      </button>
+   
+     <Link to="/member/create">
+        <button className="text-xl py-2 px-7 bg-sky-500/100 text-white rounded-lg shadow-md hover:bg-sky-600 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75 mb-5">
+          Create
+        </button>
+      </Link>
 
-      <table className="table-auto w-full"> 
+
+
+      <table className="w-full bg-white cursor-pointer   rounded-2xl overflow-hidden"> 
         <thead>
           <tr className="border-4 border-zinc-300  bg-gray-200 text-slate-600">
             <th className="px-4 py-4">Action</th>
@@ -36,7 +42,9 @@ const MemberPage = () => {
           {members.map((member, index) => (
             <tr className="border-4 border-zinc-300 text-slate-500" key={index}>
               <td className="px-4 py-2">
+                <Link to={`/detail/${member.id}`} >
                 <button class="py-1 px-7 bg-sky-500/100 text-white rounded-lg shadow-md hover:bg-sky-600 focus:ring focus:ring-violet-400 focus:ring-opacity-75 mb-5">view</button>
+                </Link>
               </td>
               <td className="px-4 py-2">{member.member_code}</td>
               <td className="px-4 py-2">{member.fullname}</td>
